2c0b6732f7438e6ae10b2a781d2effe27e3a8958
XanderRoy <Xanendorf@gmail.com>
2018-11-28 23:05:12 -0600

    render table in js

--- static/js/app.js
+++ static/js/app.js
@@ -4,18 +4,30 @@ var ufoReports = data;
 
 // rendering table
 var table = d3.select('#ufo-table');
+var thead = d3.select('thead')
 var tbody = d3.select('tbody');
-var thead = d3.select('thead');
 
-ufoReports.forEach((ufoReport) => {
-    var row = tbody.append("tr")
-    Object.entries(ufoReport).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
+function createTable() {
+  
+  var headers = ['Date', 'City', 'State', 'Country', 'Shape', 'Duration', 'Comments']
+  var row = thead.append("tr")
+
+  headers.forEach((value) => {
+  console.log(value)
+  var cell = row.append("td");
+  cell.text(value); })
+
+
+  ufoReports.forEach((ufoReport) => {
+      var row = tbody.append("tr")
+      Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+      });
     });
-  });
-console.log("Table Rendered");
-
+  console.log("Table Rendered");
+};
+createTable();
 var filterButton = d3.select('#filter-btn');
 var inputElement = d3.select("#form-control");
 
